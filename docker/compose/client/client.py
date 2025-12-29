80e8f0b5c3e5a3e99d26e073b01788990aa5283c
Jerome Steunenberg <jerome@creativemoods.pt>
2025-12-29 17:43:50 +0000

    httproutes

--- docker/compose/client/client.py
+++ docker/compose/client/client.py
@@ -1,21 +1,30 @@
 import os
-import mysql.connector
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import pymysql
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        mydb= mysql.connector.connect(user=os.environ["MYSQL_USER"],
+        conn = pymysql.connect(
+            user=os.environ["MYSQL_USER"],
             password=os.environ["MYSQL_PASSWORD"],
             host=os.environ["MYSQL_HOST"],
-            database=os.environ["MYSQL_DB"])
-        mycursor = mydb.cursor()
-        mycursor.execute("SELECT * FROM user")
-        myresult = mycursor.fetchall()
-        for user in myresult:
-            self.wfile.write(user[1] + "\n")
-        mydb.close()
+            database=os.environ["MYSQL_DB"],
+        )
+        try:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM user")
+                rows = cur.fetchall()
 
-httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+
+            for row in rows:
+                # row[1] matches your original intent; ensure bytes for wfile
+                self.wfile.write((str(row[1]) + "\n").encode("utf-8"))
+        finally:
+            conn.close()
+
+
+if __name__ == "__main__":
+    HTTPServer(("", 8000), Handler).serve_forever()
